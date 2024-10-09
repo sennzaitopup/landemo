b1af75251b971c46beaed56fbf9d22846a45adb3
Sennzai <sennzaitopup@gmail.com>
2024-10-09 13:31:28 +0700

    Move : Page.tsx

---
+++ app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/navbar";
+
+export default function page () {
+
+  return (
+    <div className="min-h-screen bg-black">
+      <div className="max-w-7xl mx-auto">
+        <Navbar/>
+      </div>
+    </div>
+  );
+    
+}
