c0a3655f328721f84fc05ae544c4e30e3c25f2d9
Sennzai <sennzaitopup@gmail.com>
2024-10-10 04:00:20 +0700

    Image config

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'sennzai.dev',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
+   
+module.exports = nextConfig;
